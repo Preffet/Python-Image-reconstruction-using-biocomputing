a15177f6f864e603963e908f5f22190f556d795b
Preffet <jstmtr@gmail.com>
2022-07-12 12:46:20 +0100

    Deleted old files

--- phoneValidator.js
+++
@@ -1,9 +0,0 @@
-function telephoneCheck(str) {
-let regex=/^(1\s?)?((\(\d{3}\)\s?)|(\d{3}[\s-]?))(\d{3}[\s-]?)(\d{4})$/
-try{
-    str.match(regex)[0];
-    return true;
-}
-catch{return false;}}
-
-telephoneCheck("1 555-555-5555");
